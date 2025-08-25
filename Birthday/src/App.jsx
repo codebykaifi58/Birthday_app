01441b0292726c661dfacf1b1e5e81e5179cbc42
codebykaifi58 <skaif1694@gmail.com>
2025-08-25 12:12:04 +0500

    birthday

--- Birthday/src/App.jsx
+++ Birthday/src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from "react";
+import "./App.css";
+import HeartBackground from "./components/HeartBackground";
+import SpecialLoader from "./components/SpeacialLoader";
+import Celebrate from "./components/Celeberate";
+import BirthdayMain from "./components/BirthdayMain";
+import Balloons from "./components/Ballon";
+import SpecialMessage from "./components/SpecialMessage";
+import MomentsCarousel from "./components/MomentCrousel";
+import FinalMessage from "./components/FinalMessage";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [step, setStep] = useState(0);
+
+  const nextStep = () => setStep(prev => prev + 1);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="app">
+      <HeartBackground />
+
+      {step === 0 && <SpecialLoader nextStep={nextStep} />}
+      {step === 1 && <Celebrate nextStep={nextStep} />}
+      {step === 2 && <BirthdayMain nextStep={nextStep} />}
+      {step === 3 && <Balloons nextStep={nextStep} />}
+      {step === 4 && <SpecialMessage nextStep={nextStep} />}
+      {step === 5 && <MomentsCarousel nextStep={nextStep} />}
+      {step === 6 && <FinalMessage />}
+    </div>
+  );
 }
 
-export default App
+export default App;
